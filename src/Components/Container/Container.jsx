827a8c0abbd680496ae1cd38b329fe45d23b96a5
KalalChandu <kalalshannu19@gmail.com>
2023-02-07 19:21:11 +0530

    update the code

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -18,6 +18,13 @@ const Container = () => {
                             placeholder='Search from the world antique collection...'     
                         />
                     </div>
+                    &nbsp;&nbsp;
+                    <div className="filters">
+                        <Filters />&nbsp;
+                        FILTERS
+                    </div>
+                    <div style={{flex: 1}}></div>
+                    <p className="surprise">🎉&nbsp;SURPRISE ME</p>
             </div>
             <div  className="sub-container">
                 <div className="box"><Roomy /></div>
